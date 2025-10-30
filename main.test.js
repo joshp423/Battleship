61cc1bdf1afcbe9c3b6cb652a85399df8cfb3231
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-10-30 23:09:58 +0000

    test file WIP

--- main.test.js
+++ main.test.js
@@ -2,7 +2,7 @@ import { occupiedGrid } from "./shipClass";
 import { Gameboard } from "./gameboardClass";
 import { describe } from "yargs";
 
-test('ship throws an error when going over game board', () => {
+test('ship is recording its coords correctly', () => {
     expect(occupiedGrid([5, 5], 2, "horizontal")).toMatchObject([[5, 5], [6, 5]]);
 });
 
@@ -10,13 +10,14 @@ test('ship throws an error when going over game board', () => {
     expect(occupiedGrid([8, 8], 2, "vertical")).toBe("Ship out of bounds, ships will always face north when set vertically.");
 });
 
-describe("creating new gameboard", () => {
+describe('creating new gameboard', () => {
 
     beforeEach(() => {
-        return new Gameboard()
+        return new Gameboard().this.Gameboard.placeship(2, "horizontal", [5, 5],)
     });
-    test('when ship is placed shows up correctly in gameboard.ships array', () => {
-        expect(Gameboard.placeship(2, "horizontal", [5, 5],))
 
+    test('when ship is placed shows up correctly in gameboard.ships array', () => {
+        expect(Gameboard.ships.length).toBe(1);
     });
+
 });
